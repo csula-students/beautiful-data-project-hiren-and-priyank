9635e766f36f7a9477e23657838de043b7441e18
hiren5050 <hphiren96@gmail.com>
2016-04-24 20:33:47 -0700

    intial

---
+++ dataScience/src/test/java/edu/csula/datascience/aquisition/MockCollector.java
@@ -0,0 +1,15 @@
+package edu.csula.datascience.aquisition;
+
+import java.util.Collection;
+
+public class MockCollector  implements Collector<SimpleModel, MockData> {
+
+	public Collection<SimpleModel> mungee(Collection<MockData> src) {
+		return null;
+	}
+
+	public void save(Collection<SimpleModel> data) {
+	
+	}
+
+}
